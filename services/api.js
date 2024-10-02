8fd0806d38537991824b5d1a89a1ea12b348b4d6
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-10-02 14:30:28 +0700

    update

--- services/api.js
+++ services/api.js
@@ -9,3 +9,15 @@ export const getAllQuiz = async ()=>{
         alert("Lỗi")
     }
 }
+
+export const getQuestionByIdQuiz = async(idQuiz)=>{
+    try {
+        // call api lấy danh sách question(câu hỏi theo id của quiz)
+        const res = await fetch(`http://localhost:3000/questions?quizId=${idQuiz}`); // call api:bất đồng bộ
+        const data = await res.json()
+        // console.log(data); // đồng bộ
+        return data;
+    } catch (error) {
+        alert("Lỗi")
+    }
+}
