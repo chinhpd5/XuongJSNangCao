d99c079b2c91e8e7585f32225b454b762b8f2e04
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-10-07 14:57:28 +0700

    check answers

--- controllers/question.js
+++ controllers/question.js
@@ -43,8 +43,8 @@ const app ={
             // 3. Thay đổi nội dung câu hỏi
             return `
                 <div class="question_item border border-2 rounded p-4 mb-2">
-                    <h4 class="question_number">Câu hỏi: ${index+1}</h4>
-                    <h5 class="question_title">
+                    <h4 class="question_number" id="${item.id}">Câu hỏi: ${index+1}</h4>
+                    <h5 class="question_title" >
                        ${item.questionTiltle}
                     </h5>
                     <div class="answer_items mt-3">
@@ -91,6 +91,16 @@ const app ={
         const btnSubmit = document.getElementById('btn_submit');
         btnSubmit.addEventListener('click',()=>{
             if(confirm("Bạn có chắc chắn nộp bài không?")){
+                // 0. disabe nút input (Người dùng không thể thay đổi đáp khi đã submit)
+                const inputAll = document.querySelectorAll('input');
+                inputAll.forEach((item)=>{
+                    // hủy hành vi mặc định của sự kiện
+                    item.addEventListener('click',(e)=>{
+                        e.preventDefault()
+                    })
+
+                })
+
                 // I. Lấy đáp án mà người lựa chọn
                 // 1. lấy tất cả câu trả lời theo từng câu hỏi
                 const listAnswersUser = document.querySelectorAll('.answer_items');
@@ -119,13 +129,82 @@ const app ={
                     if(data.idAnswers && data.idAnswers.length)
                         listAnswerSubmit.push(data)
                 })
-                console.log(listAnswerSubmit);
+                // console.log(listAnswerSubmit);
+                // Kiểm tra đáp xem có chính xác không
+                this.checkAnswers(listAnswerSubmit)
                 
             }
             
         })
     },
+    checkAnswers: function(listAnswerSubmit){
+        // 1. Lưu trữ kết quả kiểm tra
+        const checkResult=[];
+        // console.log(listAnswerSubmit);
+        console.log(listQuestion); // danh sách câu hỏi từ getQuizandQuestion
+        
+        // 2. duyệt qua các đáp án mà người dùng lựa chọn
+        const listStatus = [];
+        let countRight =0;
 
+        listAnswerSubmit.forEach((ansUser)=>{
+            // ansUser
+            // console.log(ansUser);
+            // 2.1 tìm câu hỏi có đáp án trong mảng listQuestion(lấy từ db)
+            const findQuestion = listQuestion.find((ques)=> {return ques.id == ansUser.idQuestion})
+            // console.log(findQuestion);
+            // 2.2 so sánh giá trị của 2 mảng
+            //  ansUser.idAnswers: danh sách đáp của user (mảng)
+            // findQuestion.correctAnser: đáp án chính xác lấy từ db (mảng)
+            const isCheck = this.checkEqual(ansUser.idAnswers,findQuestion.correctAnser);
+            // 2.3 Lưu trữ trạng thái đúng/sai của câu hỏi
+
+            if(isCheck){
+                // nếu đúng tăng count lên 1
+                countRight++
+            }
+            // lưu trữ trạng thái đúng hoặc sai của câu hỏi đã trả lời
+            listStatus.push({
+                idQuestion: findQuestion.id,
+                status: isCheck
+            })
+        })
+        // hiên thị trạng thaid đúng hoặc sai của câu hỏi đã trả lời
+        this.renderStatus(listStatus);
+        // thông báo
+        alert(`Ban tra loi dung ${countRight}/${listQuestion.length}`)
+        // console.log(listStatus);
+
+    },
+    checkEqual: function(arr1,arr2){
+        // kiểm tra xem 2 mảng có bằng nhau hay không
+        //1. kiểm tra độ dài của 2 mảng
+        if( arr1.length != arr2.length)
+            return false
+
+        // 2. kiểm tra giá trị
+        // arr1 = [1,2,3]
+        // arr2 = [1,2,3]
+        // 2.1 xắp xếp thứ tự 2 mảng tăng hoặc giảm dần
+        arr1 = arr1.sort();
+        arr2 = arr2.sort();
+        // console.log(arr1);
+        // console.log(arr2);
+        // 2.2 check đáp án
+        for(var i =0; i< arr1.length;i++){
+            if(arr1[i] != arr2[i]){
+                return false
+            }
+        }
+        // nêu độ dài bằng nhau và đáp án giống nhau
+        return true;
+    },
+    renderStatus: function(listStatus){
+        listStatus.forEach((item)=>{
+            const title = document.getElementById(item.idQuestion);
+            title.innerHTML = `${title.textContent} ${item.status ? `<span class="badge text-bg-success">Đúng</span>`: `<span class="badge text-bg-danger">Sai</span>`}`
+        })
+    },
     start: function(){
         this.getQuizandQuestion();
         this.handleSubmit();
